e7f3d265aca160b5da9ec12acdb2d2d49719acab
yangruigithub <727545512@qq.com>
2016-09-23 22:57:54 +0800

    Error:cannot be fixed

--- js/exh_shadow.js
+++ js/exh_shadow.js
@@ -5,7 +5,7 @@ for(j=0;j<label_shadow.length;j++)   //阻塞函数bug
 	console.log(j);
 	(function (j){
 			aLi[j].onmouseover=function () {
-				label_shadow[i].style.box-shadow= '10px 10px 5px #888888';
+				label_shadow[j].style.box-shadow= '10px 10px 5px #888888';
 			}
 	})(j);
 }
